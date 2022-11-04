9f9a60f4e58ee878ef07d839478de678797fdbf0
FrancisPj <francis.jub@free.fr>
2022-11-04 19:19:58 +0100

    first commit

---
+++ front/js/product.js
@@ -0,0 +1,8 @@
+// ETAPE 1 : Récupérer l'id dans l'URL
+// https://www.sitepoint.com/get-url-parameters-with-javascript/
+
+// ETAPE 2 : Récupérer les infos de l'api pour le produit ( ID : xxxx , fetch pour l'article xxx)
+// fetch("localhost:3000/api/products/Id-De-Ton-Produit)
+
+// ETAPE 3 : Afficher les informations du fetch dans le DOM
+
---
+++ front/js/script.js
@@ -0,0 +1,47 @@
+let items = document.getElementById('items');
+
+
+function getProductList() {
+    fetch("http://localhost:3000/api/products")
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function (products) {
+            for (let product of products) {
+                insertProduct(product)
+            }
+        })
+}
+
+
+function insertProduct(product) {
+
+    let anchor = document.createElement('a');
+    anchor.href = "./product.html?id=" + product._id // ./product.html?id=xxxxxxx
+
+    let article = document.createElement('article');
+
+    let img = document.createElement('img');
+    img.src = product.imageUrl;
+    img.alt = product.altTxt;
+
+    let title = document.createElement('h3');
+    title.innerText = product.name;
+    title.classList.add = "productName";
+
+    let description = document.createElement('p');
+
+    description.innerText = product.description;
+    description.classList.add = "productDescription";
+
+    article.appendChild(img);
+    article.appendChild(title);
+    article.appendChild(description)
+    anchor.appendChild(article);
+
+    items.appendChild(anchor);
+}
+
+getProductList()
